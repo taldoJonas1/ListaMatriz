9b887645bbd00874fbf409df798cbf06c2d9c300
João Pucci <126732888+taldoJonas1@users.noreply.github.com>
2023-10-17 20:11:09 -0300

    Update exemplos.js

--- exemplos.js
+++ exemplos.js
@@ -34,3 +34,22 @@ let soma2 = (matriz) => {
     }
     console.log(soma2)
 }
+
+====================================================================================
+    
+let exe01 = () => {
+    let matriz = []
+    let i
+    leituraMatriz(matriz)
+    resposta(matriz)
+}
+
+let leituraMatriz = (mat) => {
+    for(i = 0; i < 3; i++){
+        mat[i] = []
+        for(i = 0; i < 5; i++){
+            mat[i][j] =  Math.floor(Math.random() * (100 - 1 + 1)) + 1
+        }
+    }
+}
+====================================================================================
